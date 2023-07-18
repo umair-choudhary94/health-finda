8893d4900cb4db48350768cad8dca21baffe8e79
Umair Choudhary <82431716+umair-choudhary94@users.noreply.github.com>
2023-07-18 18:47:05 +0500

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "healthfinda.apps.HealthfindaConfig",
+    "Registration.apps.RegistrationConfig"
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("healthfinda.urls"))
+    path("",include("healthfinda.urls")),
+    path("registration/",include("Registration.urls")),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
